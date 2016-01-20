8f7b4a8be1d6bb8937b8539512895f3eadc92e7e
insalada <insalada@gmail.com>
2016-01-20 22:24:31 +0100

    first commit

---
+++ src/main/java/com/zitro/casino/Casino.java
@@ -0,0 +1,9 @@
+package com.zitro.casino;
+
+public class Casino {
+
+	public static void main(String[] args) {
+		System.out.println("Bienvenido al Casino de ZitroGames");
+	}
+
+}
---
+++ src/test/java/com/zitro/casino/CasinoTest.java
@@ -0,0 +1,21 @@
+package com.zitro.casino;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Tests for the 
+ * @author insalada
+ *
+ */
+public class CasinoTest {
+	
+	private String test = null;
+	
+	@Test
+	public void testMain() {
+		assertTrue(test==null);
+	}
+	
+	
+}
